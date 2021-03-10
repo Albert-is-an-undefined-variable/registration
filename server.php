d10e53cafbc0eb5581ea92866935a5be675b38ce
justalilibit <lilianboll@mailbox.org>
2021-03-10 18:01:53 +0100

    Trying to get the f**k**g entry running

--- server.php
+++ server.php
@@ -6,9 +6,9 @@ $username = "";
 $email    = "";
 $errors = array();
 $samplename = $celltype = $idfreezer = $rack = $position = $amount = $frozendate = $availability = '';
-$errors_registration = array('username' => '', 'email' => '', 'password_1' => '', 'password_2' => '');
+
 // connect to the database
-$db = mysqli_connect('localhost', 'albert', '/Puiyuaru1616', 'mydb');
+$db = mysqli_connect('localhost', 'tidytubes', 'Welcome123%', 'mydb');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -20,13 +20,13 @@ if (isset($_POST['reg_user'])) {
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { $errors_registration['username'] = "Username is required"; }
-  else { if (!preg_match('/^[a-zA-Z\s]+$/', $username)){ $errors_registration['username'] = "Your user name cannot contain special characters"; }}
-  if (empty($email)) { $errors_registration['email'] = "Email is required"; }
-  // if (empty($password_1)) { array_push($errors, "Password is required"); }
-  else { if (strlen($password_1) <= 8 ){ $errors_registration['password_1'] = "Your password must have at least 8 characters"; }}
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  else { if (!preg_match('/^[a-zA-Z\s]+$/', $username)){ array_push($errors, "Your user name cannot contain special characters"); }}
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  else { if (strlen($password_1) <= 7 ){ array_push($errors, "Password must have at least 8 characters"); }}
   if ($password_1 != $password_2) {
-    $errors_registration['password_2'] = "The two passwords do not match";
+    array_push($errors, "The two passwords do not match");
   }
 
   // first check the database to make sure
@@ -36,20 +36,17 @@ if (isset($_POST['reg_user'])) {
   $user = mysqli_fetch_assoc($result);
 
   if ($user) { // if user exists
-    if ($user['Username'] === $username) {
-      $errors_registration['username'] = "Username already exists";
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
     }
 
-    if ($user['Email'] === $email) {
-      $errors_registration['email'] = "email already exists";
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
     }
   }
 
   // Finally, register user if there are no errors in the form
-  if(array_filter($errors_registration)){
-
-  } else {
-
+  if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO User (Username, Email, Password)
@@ -65,65 +62,67 @@ if (isset($_POST['reg_user'])) {
 // LOGIN USER
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username)) {
-  	$errors_registration['username'] = "Username is required";
+  	array_push($errors, "Username is required");
   }
   if (empty($password)) {
-  	$errors_registration['password_1'] = "Password is required";
+  	array_push($errors, "Password is required");
   }
 
-  // Finally, register user if there are no errors in the form
-  if(array_filter($errors_registration)){
-
-  } else {
-
+  if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM User WHERE Username='$username' AND Password='$password'";
+  	$query = "SELECT * FROM Users WHERE Username='$username' AND Password='$password'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
   	  $_SESSION['success'] = "You are now logged in";
   	  header('location: index.php');
   	}else {
-  		$errors_registration['password_1'] = "Wrong username/password combination";
+  		array_push($errors, "Wrong username/password combination");
   	}
   }
 }
 
 
 // NEW ENTRY
+
 if (isset($_POST['reg_entry'])) {
   // receive all input values from the entry form
   $samplename = mysqli_real_escape_string($db, $_POST['samplename']);
   $celltype = mysqli_real_escape_string($db, $_POST['celltype']);
-  // $idfreezer = mysqli_real_escape_string($db, $_POST['idfreezer']);
+  $idfreezer = mysqli_real_escape_string($db, $_POST['idfreezer']);
   $rack = mysqli_real_escape_string($db, $_POST['rack']);
   $position = mysqli_real_escape_string($db, $_POST['position']);
-  // $amount = mysqli_real_escape_string($db, $_POST['amount']);
+  $amount = mysqli_real_escape_string($db, $_POST['amount']);
   $frozendate = mysqli_real_escape_string($db, $_POST['frozendate']);
   $availability = mysqli_real_escape_string($db, $_POST['availability']);
   $comment = mysqli_real_escape_string($db, $_POST['comment']);
+  $idSampleTest = 1;
+  $idUserTest = 1;
 
   // entry validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($samplename)) { array_push($errors, "Sample name is required"); }
-  if (empty($celltype)) { array_push($errors, "Cell type is required"); }
+  // if (empty($celltype)) { array_push($errors, "Cell type is required"); }
   // if (empty($idfreezer)) { array_push($errors, "Freezer is required"); }
-  if (empty($rack)) { array_push($errors, "Rack is required"); }
-  if (empty($position)) { array_push($errors, "Position is required"); }
+  // if (empty($rack)) { array_push($errors, "Rack is required"); }
+  // if (empty($position)) { array_push($errors, "Position is required"); }
   // if (empty($amount)) { array_push($errors, "Amount is required"); }
-  if (empty($frozendate)) { array_push($errors, "Frozen date is required"); }
+  // if (empty($frozendate)) { array_push($errors, "Date is required"); }
 
   // Finally, add the new entry in the sample table
+  // if (count($errors) == 0) {
+  // 	$query = "INSERT INTO Sample (Name, Cell_type, Rack, Position, Frozendate, Availability, Comment, idUser)
+  // 			  VALUES('$samplename', '$celltype', '$rack', '$position', '$frozendate', '$availability', '$comment', 1)";
+  // 	mysqli_query($db, $query);
+  // }
   if (count($errors) == 0) {
-  	$query = "INSERT INTO Sample (Name, Cell_type, Rack, Position, Frozendate, Availability, Comment, User_Email)
-  			  VALUES('$samplename', '$celltype', '$rack', '$position', '$frozendate', '$availability', '$comment', '$email')";
+  	$query = "INSERT INTO Sample (idSample, Name, Cell_type, idUser)
+  			  VALUES('$idSampleTest', '$idUserTest')";
   	mysqli_query($db, $query);
-
   }
 }
-
 
 ?>
